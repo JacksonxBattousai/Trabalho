9260b444793b2c4791f97c031aa919b169467e94
JacksonxBattousai <JacksonxBattousai@users.noreply.github.com>
2018-03-30 11:37:27 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { UsertableComponent } from './components/usertable/usertable.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsertableComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
